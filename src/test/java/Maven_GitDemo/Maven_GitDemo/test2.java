4dadb9d79433da8e76783da60bc4172975b29f28
Garvita <garvita160109@gmail.com>
2020-12-30 23:12:10 -0600

    Commit Test2 to BranchX

---
+++ src/test/java/Maven_GitDemo/Maven_GitDemo/test2.java
@@ -0,0 +1,14 @@
+package Maven_GitDemo.Maven_GitDemo;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		String s1 = "Good Job";
+		String s2 = "Finally";
+		s2 = "garvita";
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+
+}
