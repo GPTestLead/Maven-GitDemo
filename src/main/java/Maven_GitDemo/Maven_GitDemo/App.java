4a95fc660351eae51df98153ce5c533fcf4000db
Garvita <garvita160109@gmail.com>
2020-12-30 22:54:22 -0600

    Maven 1st commit

---
+++ src/main/java/Maven_GitDemo/Maven_GitDemo/App.java
@@ -0,0 +1,13 @@
+package Maven_GitDemo.Maven_GitDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Maven_GitDemo/Maven_GitDemo/test1.java
@@ -0,0 +1,14 @@
+package Maven_GitDemo.Maven_GitDemo;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		String s1 = "java";
+		String s2 = "java";
+		s2 = "garvita";
+		System.out.println(s1);
+		System.out.println(s2);
+
+	}
+
+}
