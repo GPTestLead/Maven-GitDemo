634941d4d15795b9b8d5e3f06ebb836fe786fb05
Garvita <garvita160109@gmail.com>
2020-12-30 23:46:34 -0600

    Added 1 check...

--- src/test/java/Maven_GitDemo/Maven_GitDemo/test2.java
+++ src/test/java/Maven_GitDemo/Maven_GitDemo/test2.java
@@ -5,10 +5,11 @@ public class test2 {
 	public static void main(String[] args) {
 		String s1 = "Good Job";
 		String s2 = "Finally";
-		s2 = "garvita";
+		String s3 = "Garvita";
+		
 		System.out.println(s1);
 		System.out.println(s2);
-
+		System.out.println(s3);
 	}
 
 }
